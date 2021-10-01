e98c5a177d847eb20118d09cea6116e60db4c4e6
Martin Achtermann <65299501+roadpilot@users.noreply.github.com>
2021-10-01 17:16:26 +0000

    refine assignments to match wiki

--- main.sql
+++ main.sql
@@ -2,4 +2,10 @@
 Consider P1(a,c) and P2(b,d) to be two points on a 2D plane where (a,b) are the respective minimum and maximum values of Northern Latitude (LAT_N) and (c,d) are the respective minimum and maximum values of Western Longitude (LONG_W) in STATION.
 
 Query the Euclidean Distance between points P1 and P2 and format your answer to display 4 decimal digits.
+
+a = min(lat_n)  = p1
+b = max(lat_n)  = q1
+c = min(long_w) = p2
+d = max(long_w) = q2
+
 */
